9113df4e2211942d7e5b68123d77a853f38c3a68
nickmi <nikolasmichalaros@gmail.com>
2018-10-22 18:42:38 +0300

    code refactoring

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 /**
  * Encrypt a message using the recipient's public key.
- * @param  {pubkey} String - Encrypted ASCII Armored public key.
+ * @param  {pubKey} String - Encrypted ASCII Armored public key.
  * @param  {message} String - Your message to the recipient.
  * @return {pgpMessage} String - Encrypted ASCII Armored message.
  */
@@ -10,7 +10,7 @@ async function encrypt_message(textAreaValues)
 {
     let options =  {
         message: openpgp.message.fromText(textAreaValues.message),
-        publicKeys: ( await openpgp.key.readArmored(textAreaValues.pubkey)).keys
+        publicKeys: ( await openpgp.key.readArmored(textAreaValues.pubKey)).keys
 };
     openpgp.encrypt(options).then(function(ciphertext){
         document.getElementById("pgpMessage").value = ciphertext.data;
@@ -49,7 +49,7 @@ async function decrypt_message(valuesForDecryption) {
 
         let zip = new JSZip();
         zip.file("privkey.asc", privkey);
-        zip.file("pubkey.asc", pubkey);
+        zip.file("pubKey.asc", pubkey);
         zip.generateAsync({type:"blob"})
             .then(function(content) {
 
@@ -71,20 +71,30 @@ function bringBackDomDefaults  ()  {
 
 function validateInput() {
 
-    let internalValuesOfDom={
-        email : $("#emailForm").val(),
-        name : $("#nameForm").val(),
-        password : $("#passForm").val(),
-
+    let valuesOfForm={
+        email:document.getElementById("emailForm").value,
+        name : document.getElementById("nameForm").value,
+        password : document.getElementById("passForm").value,
+        checkIfEmptyValues: function() {
+            return (this.email&&this.name&&this.password)==="";
+        }
     };
 
-    $("#keygenerationButton").prop('disabled', (internalValuesOfDom.email&&internalValuesOfDom.name&&internalValuesOfDom.password)==="");
+    //$("#keygenerationButton").prop('disabled', (valuesOfForm.checkIfEmptyValues()));
+
+    document.getElementById('keygenerationButton').disabled = valuesOfForm.checkIfEmptyValues();
+
 }
 
 $(document).ready(function(){
 
 
-    $("#keygenerationButton").prop('disabled', true);
+   // $("#keygenerationButton").prop('disabled', true);
+
+    //const el = document.querySelector('.form');
+    document.getElementById('keygenerationButton').disabled = true;
+
+
 
     $("#emailForm,#nameForm,#passForm").each(function(){
         $(this).keypress(validateInput).keyup(validateInput).keydown(validateInput);
@@ -94,8 +104,8 @@ $(document).ready(function(){
 
         let valuesForDecryption = {
 
-             message : $('textarea#pgpMessage').val(),
-             privateKey : $('textarea#pgpKey').val(),
+             message : document.getElementById("pgpMessage").value,
+             privateKey : document.getElementById("pgpKey").value,
              passphrase: document.getElementById("passphrasePGP").value
 
         };
@@ -108,8 +118,8 @@ $(document).ready(function(){
 
         let textAreaValues= {
 
-            message : $('textarea#plainTextMessage').val(),
-            pubkey :  $('textarea#pgpKey').val()
+            message : document.getElementById("plainTextMessage").value,
+            pubKey  : document.getElementById("pgpKey").value
     };
             encrypt_message(textAreaValues);
 
